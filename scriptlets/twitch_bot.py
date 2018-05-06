9284cdb34393de31e52b7a2872e6057640428ad7
Mark Seiden <mwseiden@gmail.com>
2018-05-06 10:10:28 -0400

    Run chat bot in thread

--- scriptlets/twitch_bot.py
+++ scriptlets/twitch_bot.py
@@ -1,4 +1,5 @@
 import os
+import threading
 from mpf.core.scriptlet import Scriptlet
 from .twitch.twitch_client import TwitchClient
 
@@ -9,7 +10,9 @@ class TwitchBot(Scriptlet):
         if enabled != None and enabled.upper() == 'TRUE':
             self.info_log('Successful connection to Twitch')
             self.client = TwitchClient(os.environ.get('TWITCH_USER'), os.environ.get('TWITCH_PASSWORD'), os.environ.get('TWITCH_CHANNEL'))
-            self.client.start()
+            thread = threading.Thread(target=self.client.start, args=())
+            thread.daemon = True
+            thread.start()
 
             if self.client.is_connected():
                 self.info_log('Successful connection to Twitch')
