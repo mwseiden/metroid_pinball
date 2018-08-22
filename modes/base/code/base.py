390c6bbe74edf6f2a61dbaaae17c57c3e43dc6d6
Mark Seiden <mwseiden@gmail.com>
2018-08-22 15:18:46 -0400

    Add horizontal sweep on chat message

--- modes/base/code/base.py
+++ modes/base/code/base.py
@@ -1,4 +1,6 @@
 from mpf.core.mode import Mode
+from mpf.core.rgb_color import RGBColor
+from ....scriptlets.lights.backbox.sweep_horizontal import SweepHorizontal
 
 class Base(Mode):
 
@@ -6,5 +8,11 @@ class Base(Mode):
         self.add_mode_event_handler('twitch_new_chat_message', self.chat_message)
 
     def chat_message(self, **kwargs):
-        for x in self.machine.scriptlets:
-            self.info_log(x)
+        self._backbox_scriptlet().set_overlay_effect(SweepHorizontal(self.machine, RGBColor([128, 64, 0]), 3, 3, 0))
+
+    # private ----------------------------------------------------------------
+
+    def _backbox_scriptlet(self):
+        for scriptlet in self.machine.scriptlets:
+            if scriptlet.name == 'BackBoxLights':
+                return scriptlet
