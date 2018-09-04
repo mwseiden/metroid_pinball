e654ee2fedd02ed2c4484f1c0d4583e89cae0876
Mark Seiden <mwseiden@gmail.com>
2018-09-04 08:26:39 -0400

    Fix show tokens

--- modes/match/code/match.py
+++ modes/match/code/match.py
@@ -9,10 +9,18 @@ class MyMatch(Match):
         self.add_mode_event_handler('has_match', self.has_match)
 
     def no_match(self, **kwargs):
-        show_tokens = dict(match_number0=kwargs.get('match_number0', ''), match_number1=kwargs.get('match_number1', ''), match_number2=kwargs.get('match_number2', ''), match_number3=kwargs.get('match_number3', ''), winner_number=kwargs.get('winner_number', ''))
-        self.machine.shows['no_match_show'].play(loops=0, show_tokens=show_tokens)
+        self.play_show('no_match_show', kwargs)
 
     def has_match(self, **kwargs):
-        show_tokens = dict(match_number0=kwargs.get('match_number0', ''), match_number1=kwargs.get('match_number1', ''), match_number2=kwargs.get('match_number2', ''), match_number3=kwargs.get('match_number3', ''), winner_number=kwargs.get('winner_number', ''))
-        self.machine.shows['has_match_show'].play(loops=0, show_tokens=show_tokens)
+        self.play_show('has_match_show', kwargs)
+
+    def play_show(self, show_name, event_params):
+        show_tokens = dict(
+            match_number0=str(event_params.get('match_number0', '')),
+            match_number1=str(event_params.get('match_number1', '')),
+            match_number2=str(event_params.get('match_number2', '')),
+            match_number3=str(event_params.get('match_number3', '')),
+            winner_number=str(event_params.get('winner_number', ''))
+        )
+        self.machine.shows[show_name].play(loops=0, show_tokens=show_tokens)
 
