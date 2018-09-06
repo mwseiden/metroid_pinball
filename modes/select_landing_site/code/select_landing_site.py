c347c3d4942411aba8c7ef06b0091bd6d8f69b35
Mark Seiden <mwseiden@gmail.com>
2018-09-06 15:49:51 -0400

    Try to dynamically insert into carousel

--- modes/select_landing_site/code/select_landing_site.py
+++ modes/select_landing_site/code/select_landing_site.py
@@ -2,9 +2,11 @@ from mpf.modes.carousel.code.carousel import Carousel
 
 class SelectLandingSite(Carousel):
 
-    def mode_init(self):
-        super().mode_init()
-
+    def _update_highlighted_item(self, direction):
         player = self.machine.game.player
-        if player['has_chosen_landing_site'] != 0:
-            self._items.insert(0, 'continue')
+        items = self._get_available_items()
+
+        if player['has_chosen_landing_site'] != 0 and 'continue' not in items:
+            items.insert(0, 'continue')
+
+        super()._update_highlighted_item(direction)
