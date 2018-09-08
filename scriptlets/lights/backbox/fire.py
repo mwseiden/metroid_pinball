be7bcdb0f35af183972eee823a2783e1dd60103e
Mark Seiden <mwseiden@gmail.com>
2018-09-08 10:43:10 -0400

    Use pre-defined start colors

--- scriptlets/lights/backbox/fire.py
+++ scriptlets/lights/backbox/fire.py
@@ -1,9 +1,20 @@
 from random import randint
+from random import choice
 from mpf.core.rgb_color import RGBColor
 from .dynamic_backbox_show import DynamicBackBoxShow
 
 class Fire(DynamicBackBoxShow):
     OFF_COLOR = RGBColor([0, 0, 0])
+    FIRE_COLORS = [
+        RGBColor([80, 80, 80]),
+        RGBColor([80, 70, 60]),
+        RGBColor([80, 60, 40]),
+        RGBColor([80, 50, 20]),
+        RGBColor([80, 40, 0]),
+        RGBColor([80, 30, 0]),
+        RGBColor([80, 20, 0]),
+        RGBColor([80, 10, 0])
+    ]
 
     def __init__(self, machine):
         super().__init__(machine)
@@ -13,8 +24,9 @@ class Fire(DynamicBackBoxShow):
 
         for strip in self.strips:
             # highlight = randint(0, 30)
-            intensity = randint(0, 80)
-            strip.set_color(strip.size - 1, RGBColor([intensity, intensity / 2, intensity / 4]))
+            # intensity = randint(0, 80)
+            # RGBColor([intensity, intensity / 2, intensity / 4])
+            strip.set_color(strip.size - 1, choice(self.FIRE_COLORS))
 
             for light_index in reversed(range(1, strip.size)):
                 strip.set_color(light_index - 1, self._decay_colors(strip.get_color(light_index), strip.get_color(light_index - 1)))
