81ea904df2c23f2d39427d9d8c71dacb823664dc
Mark Seiden <mwseiden@gmail.com>
2018-09-08 09:54:16 -0400

    Debugging info for fire light show

--- scriptlets/lights/backbox/fire.py
+++ scriptlets/lights/backbox/fire.py
@@ -3,7 +3,6 @@ from mpf.core.rgb_color import RGBColor
 from .dynamic_backbox_show import DynamicBackBoxShow
 
 class Fire(DynamicBackBoxShow):
-    FIRE_COLOR = RGBColor([80, 35, 0])
     OFF_COLOR = RGBColor([0, 0, 0])
 
     def __init__(self, machine):
@@ -11,13 +10,23 @@ class Fire(DynamicBackBoxShow):
 
     def animate(self):
         super().animate()
+
+        if self.frame > 1:
+            return
+
         for strip in self.strips:
+            highlight = randint(0, 30)
+            introduced_color = RGBColor([intensity, highlight, highlight / 2])
+            self.info_log('WTF: Introduced color for frame %s, index 0: %s', self.frame, introduced_color)
+            strip.set_color(0, introduced_color)
             for light_index in range(1, strip.size - 1):
-                strip.set_color(light_index + 1, self._average_colors(strip.get_color(light_index),strip.get_color(light_index + 1)))
+                averaged_color = self._average_colors(strip.get_color(light_index), strip.get_color(light_index + 1))
+                self.info_log('WTF: Averaged colors for frame %s, index %s: %s', self.frame, light_index, averaged_color)
+                strip.set_color(light_index + 1, averaged_color)
             intensity = randint(0, 120)
-            highlight = randint(0, 60)
-            strip.set_color(0, RGBColor([intensity, highlight, highlight]))
-            strip.set_color(strip.size - 1, self._average_colors(strip.get_color(strip.size - 1), self.OFF_COLOR))
+            last_color = self._average_colors(strip.get_color(strip.size - 1), self.OFF_COLOR)
+            self.info_log('WTF: Last color for frame %s, index %s: %s', self.frame, strip.size - 1, last_color)
+            strip.set_color(strip.size - 1, last_color)
 
     def _average_colors(self, a, b):
         r1, g1, b1 = a.rgb
