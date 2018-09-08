77ed8a66c2d8970950d9d5bff55e401c9c65cc33
Mark Seiden <mwseiden@gmail.com>
2018-09-08 10:24:28 -0400

    Tweak fire algorithm

--- scriptlets/lights/backbox/fire.py
+++ scriptlets/lights/backbox/fire.py
@@ -12,17 +12,17 @@ class Fire(DynamicBackBoxShow):
         super().animate()
 
         for strip in self.strips:
-            highlight = randint(0, 30)
+            # highlight = randint(0, 30)
             intensity = randint(0, 120)
-            strip.set_color(0, RGBColor([intensity, highlight, highlight / 2]))
+            strip.set_color(strip.size - 1, RGBColor([intensity, intensity / 2, intensity / 4]))
 
-            for light_index in range(0, strip.size - 1):
-                strip.set_color(light_index + 1, self._decay_colors(strip.get_color(light_index), strip.get_color(light_index + 1)))
+            for light_index in reversed(range(1, strip.size)):
+                strip.set_color(light_index - 1, self._decay_colors(strip.get_color(light_index), strip.get_color(light_index - 1)))
 
-            strip.set_color(strip.size - 1, self._decay_colors(strip.get_color(strip.size - 1), self.OFF_COLOR))
+            strip.set_color(0, self._decay_colors(strip.get_color(0), self.OFF_COLOR))
 
     def _decay_colors(self, a, b):
         r1, g1, b1 = a.rgb
         r2, g2, b2 = b.rgb
 
-        return RGBColor([(r1 + r2) / 3, (g1 + g2) / 3, (b1 + b2) / 3])
+        return RGBColor([(r1 + r2) / 2, (g1 + g2) / 2, (b1 + b2) / 2])
