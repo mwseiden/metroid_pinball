fcf6a363591658b490a439088c8062251b41568a
Mark Seiden <mwseiden@gmail.com>
2019-07-22 20:49:08 -0400

    Add no music code

--- modes/attract/code/attract.py
+++ modes/attract/code/attract.py
@@ -19,6 +19,7 @@ class MyAttract(Attract):
         self.add_mode_event_handler('s_left_flipper_inactive', self.event_left_flipper_up)
 
         self.machine.set_machine_var('metroid_game_booted', 1)
+        self.machine.set_machine_var('next_game_acapella', 0)
 
     def event_right_flipper_down(self, **kwargs):
         self.right_flipper_down = True
@@ -63,3 +64,4 @@ class MyAttract(Attract):
         elif self.flip_pattern == 'BLLRRRLLLL':
             self.info_log('Flipper Code: NO MUSIC')
             self.machine.events.post('flipper_code_no_music')
+            self.machine.set_machine_var("next_game_acapella", 1)
