8689f6a53abfca8491a658f2881148cc49b26f55
Mark Seiden <mwseiden@gmail.com>
2019-08-19 19:58:18 -0400

    Add code

---
+++ modes/lanes/code/lanes.py
@@ -0,0 +1,13 @@
+from mpf.core.mode import Mode
+
+class Lanes(Mode):
+
+    def mode_start(self, **kwargs):
+        super().mode_start(**kwargs)
+
+        self.add_mode_event_handler('left_ballsave_shot_hit', self.event_add_a_ball)
+        self.add_mode_event_handler('right_ballsave_shot_hit', self.event_add_a_ball)
+
+    def event_add_a_ball(self, **kwargs):
+        self.machine.playfield.add_ball()
+
