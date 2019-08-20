9d8e226d05f11d1624e96a9be3b04aee6ce542b3
Mark Seiden <mwseiden@gmail.com>
2019-08-19 21:25:50 -0400

    This works

--- modes/lanes/code/lanes.py
+++ modes/lanes/code/lanes.py
@@ -9,9 +9,9 @@ class Lanes(Mode):
         self.add_mode_event_handler('right_ballsave_shot_hit', self.event_add_a_ball)
 
     def event_add_a_ball(self, **kwargs):
-        #for ball_save in self.machine.ball_saves:
-        #    if ball_save.enabled:
-        #        return
+        for ball_save in self.machine.ball_saves:
+            if ball_save.enabled:
+                return
 
         if self.machine.game.balls_in_play < 4:
             self.machine.game.balls_in_play += 1
