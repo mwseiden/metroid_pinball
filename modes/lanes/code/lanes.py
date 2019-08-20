b891183e5e3bf9b9a14015c37a94f8e488739934
Mark Seiden <mwseiden@gmail.com>
2019-08-19 20:46:13 -0400

    Don't save if ball save

--- modes/lanes/code/lanes.py
+++ modes/lanes/code/lanes.py
@@ -9,6 +9,11 @@ class Lanes(Mode):
         self.add_mode_event_handler('right_ballsave_shot_hit', self.event_add_a_ball)
 
     def event_add_a_ball(self, **kwargs):
-        self.machine.playfield.add_ball()
-        self.machine.game.balls_in_play += 1
+        for ball_save in self.machine.ball_saves:
+            if ball_save.enabled:
+                return
+
+        if self.machine.game.balls_in_play < 4
+            self.machine.game.balls_in_play += 1
+            self.machine.playfield.add_ball()
 
