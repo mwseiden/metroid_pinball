d01d74cd73e6f6940e8765f1336fd03c77fa0873
Mark Seiden <mwseiden@gmail.com>
2019-10-07 21:09:43 -0400

    Save Drop Target State

--- modes/drop_targets/code/drop_targets.py
+++ modes/drop_targets/code/drop_targets.py
@@ -7,6 +7,11 @@ class DropTargets(Mode):
 
         self.add_mode_event_handler('drop_targets_continue', self.event_restore_drop_state)
 
+    def mode_stop(self, **kwargs):
+        super().mode_stop(**kwargs)
+
+        self.machine.game.player['drops_{}'.format(player['continue_room'])] = self.drop_status('drop_one') + self.drop_status('drop_two') + self.drop_status('drop_three')
+
     def event_restore_drop_state(self, **kwargs):
         player = self.machine.game.player
         pattern = player['drops_{}'.format(player['continue_room'])] or 'UUU'
@@ -14,3 +19,6 @@ class DropTargets(Mode):
         self.machine.events.post('drop_targets_start_one' if pattern[0] == 'U' else 'cmd_drop_one_down')
         self.machine.events.post('drop_targets_start_two' if pattern[1] == 'U' else 'cmd_drop_two_down')
         self.machine.events.post('drop_targets_start_three' if pattern[2] == 'U' else 'cmd_drop_three_down')
+
+    def drop_status(self, name):
+        return 'U' if self.machine.drop_targets[name].up else 'D'
