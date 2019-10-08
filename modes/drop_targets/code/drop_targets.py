ff26a97b967a9509a9febce5c8003b55ff951a5e
Mark Seiden <mwseiden@gmail.com>
2019-10-07 20:19:52 -0400

    Add initial state to drops

---
+++ modes/drop_targets/code/drop_targets.py
@@ -0,0 +1,16 @@
+from mpf.core.mode import Mode
+
+class DropTargets(Mode):
+
+    def mode_start(self, **kwargs):
+        super().mode_start(**kwargs)
+
+        self.add_mode_event_handler('drop_targets_continue', self.event_restore_drop_state)
+
+    def event_restore_drop_state(self, **kwargs):
+        player = self.machine.game.player
+        pattern = player['drops_{}'.format(player['continue_room'])] or 'UUU'
+
+        self.machine.events.post('cmd_drop_one_up' if  pattern[0] == 'U' else 'cmd_drop_one_down')
+        self.machine.events.post('cmd_drop_two_up' if  pattern[1] == 'U' else 'cmd_drop_two_down')
+        self.machine.events.post('cmd_drop_three_up' if  pattern[2] == 'U' else 'cmd_drop_three_down')
