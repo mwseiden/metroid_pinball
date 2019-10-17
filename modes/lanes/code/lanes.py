fc82c4fdb7160a92756f966c9fcbbb2c5a792c0e
Mark Seiden <mwseiden@gmail.com>
2019-10-16 21:15:25 -0400

    Fix lane collect event

--- modes/lanes/code/lanes.py
+++ modes/lanes/code/lanes.py
@@ -7,15 +7,17 @@ class Lanes(Mode):
 
         self.add_mode_event_handler('left_ballsave_shot_hit', self.event_add_a_ball)
         self.add_mode_event_handler('right_ballsave_shot_hit', self.event_add_a_ball)
+        self.add_mode_event_handler('lanes_collect_ball_save', self.event_add_a_ball)
 
     def event_add_a_ball(self, **kwargs):
         for ball_save in self.machine.ball_saves:
             if ball_save.enabled:
                 return
 
+        self.player.lanes_last_shot = 0
+
         if self.machine.game.balls_in_play < 4:
             self.machine.game.balls_in_play += 1
             self.machine.playfield.add_ball()
             self.player.kickbacks -= 1
-            self.player.lanes_last_shot = 0
 
