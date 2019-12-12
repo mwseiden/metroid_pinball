f3c7eaa65e20ef5e26f52fba56845d27498fbef4
Mark Seiden <mwseiden@gmail.com>
2019-12-11 22:25:49 -0500

    Use correct events

--- modes/bao_lanes/code/bao_lanes.py
+++ modes/bao_lanes/code/bao_lanes.py
@@ -5,9 +5,9 @@ class BaoLanes(Mode):
     def mode_start(self, **kwargs):
         super().mode_start(**kwargs)
 
-        self.add_mode_event_handler('left_ballsave_shot_hit', self.event_add_a_ball)
-        self.add_mode_event_handler('right_ballsave_shot_hit', self.event_add_a_ball)
-        self.add_mode_event_handler('lanes_collect_ball_save', self.event_add_a_ball)
+        self.add_mode_event_handler('bao_left_ballsave_shot_hit', self.event_add_a_ball)
+        self.add_mode_event_handler('bao_right_ballsave_shot_hit', self.event_add_a_ball)
+        self.add_mode_event_handler('bao_lanes_collect_ball_save', self.event_add_a_ball)
 
     def event_add_a_ball(self, **kwargs):
         for ball_save in self.machine.ball_saves:
