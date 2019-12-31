410c595f827960bbd1fc52ef04e9b2521b7d1a5c
Mark Seiden <mwseiden@gmail.com>
2019-12-30 22:56:31 -0500

    Now mode init

--- modes/room_2f_frozen_lake/code/room_2f_frozen_lake.py
+++ modes/room_2f_frozen_lake/code/room_2f_frozen_lake.py
@@ -3,10 +3,10 @@ from mpf.core.mode import Mode
 
 class Room2fFrozenLake(Mode):
 
-    def mode_start(self, **kwargs):
-        self.add_mode_event_handler('room_2f_choose_next_shots', self.event_add_a_shot)
+    def mode_init(self, **kwargs):
+        self.add_mode_event_handler('room_2f_choose_first_shots', self.event_add_a_shot)
 
-        super().mode_start(**kwargs)
+        super().mode_init(**kwargs)
 
     def event_add_a_shot(self, **kwargs):
         self.machine.events.post('room_2f_enable_shot_{}'.format(randint(1, 11)))
