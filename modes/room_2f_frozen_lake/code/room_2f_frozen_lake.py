e2d3f8d2c74b29d1d69f1e1fbda0695fa5756a5b
Mark Seiden <mwseiden@gmail.com>
2020-01-01 17:10:32 -0500

    Modify python code

--- modes/room_2f_frozen_lake/code/room_2f_frozen_lake.py
+++ modes/room_2f_frozen_lake/code/room_2f_frozen_lake.py
@@ -5,8 +5,7 @@ from mpf.core.mode import Mode
 class Room2fFrozenLake(Mode):
 
     def mode_start(self, **kwargs):
-        player = self.machine.game.player
-        player['room_2f_target_order'] = shuffle(player['room_2f_target_order'])
+        self.player.room_2f_target_order = shuffle(self.player.room_2f_target_order)
 
         self.add_mode_event_handler('room_2f_choose_next_shot', self.event_add_a_shot)
         self.event_add_a_shot(**kwargs)
@@ -15,6 +14,6 @@ class Room2fFrozenLake(Mode):
 
     def event_add_a_shot(self, **kwargs):
         if len(player['room_2f_target_order']) > 0:
-            next_shot = ord(player['room_2f_target_order'][0]) - 64
-            player['room_2f_target_order'] = player['room_2f_target_order'][1:]
+            next_shot = ord(self.player.room_2f_target_order[0]) - 64
+            self.player.room_2f_target_order = self.player.room_2f_target_order[1:]
             self.machine.events.post('room_2f_enable_shot_{}'.format(next_shot))
