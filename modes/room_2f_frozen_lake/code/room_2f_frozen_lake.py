29349ac27121f8db038a912f3f1d2a45ed3eb020
Mark Seiden <mwseiden@gmail.com>
2020-01-06 21:15:39 -0500

    Add scoop logic

--- modes/room_2f_frozen_lake/code/room_2f_frozen_lake.py
+++ modes/room_2f_frozen_lake/code/room_2f_frozen_lake.py
@@ -6,21 +6,9 @@ class Room2fFrozenLake(Mode):
 
     def mode_start(self, **kwargs):
         self.add_mode_event_handler('room_2f_choose_next_shot', self.event_add_a_shot)
+        self.add_mode_event_handler('room_2f_reset_shots', self.event_reset_shots)
 
-        player = self.machine.game.player
-
-        # if shot sequence is 0 characters long then reset pattern to default
-        if len(player['room_2f_target_order']) == 0:
-            player['room_2f_target_order'] = 'ABCDEFGHIJKLMN'
-
-        # if shot sequence is 14 characters long
-        #    shuffle first 10
-        #    shuffle last 4
-        self.warning_log("WTF %s", len(player['room_2f_target_order']))
-        if len(player['room_2f_target_order']) == 14:
-            player['room_2f_target_order'] = self.shuffle_shots(player['room_2f_target_order'][:10]) + self.shuffle_shots(player['room_2f_target_order'][10:])
-            self.warning_log("WTF %s", player['room_2f_target_order'])
-
+        self.event_reset_shots(**kwargs)
         self.event_add_a_shot(**kwargs)
 
         super().mode_start(**kwargs)
@@ -28,16 +16,18 @@ class Room2fFrozenLake(Mode):
     def event_add_a_shot(self, **kwargs):
         player = self.machine.game.player
 
-        # only if shots are left:
-        #   pop off first character
-        #   subtract 64 and save as next shot
-        #   save abbreviated shot list
         if len(player['room_2f_target_order']) > 0:
             next_shot = ord(player['room_2f_target_order'][:1]) - 64
             player['room_2f_target_order'] = player['room_2f_target_order'][1:]
             self.machine.events.post('room_2f_enable_shot_{}'.format(next_shot))
+        else:
+            self.machine.events.post('room_2f_enable_scoop_shot'.format(next_shot))
+
+
+    def event_reset_shots(self, **kwargs):
+        player = self.machine.game.player
+        player['room_2f_target_order'] = 'ABCDEFGHIJKLMN'
+        player['room_2f_target_order'] = self.shuffle_shots(player['room_2f_target_order'][:10]) + self.shuffle_shots(player['room_2f_target_order'][10:])
 
     def shuffle_shots(self, s):
-        self.warning_log("WTF %s", s)
-        self.warning_log("WTF %s", ''.join(sample(s,len(s))))
         return ''.join(sample(s,len(s)))
