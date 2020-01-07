99547a8ce732e003f1024e0b0aa1a72f68aadd4a
Mark Seiden <mwseiden@gmail.com>
2020-01-06 19:27:29 -0500

    Add new code

--- modes/room_2f_frozen_lake/code/room_2f_frozen_lake.py
+++ modes/room_2f_frozen_lake/code/room_2f_frozen_lake.py
@@ -8,7 +8,21 @@ class Room2fFrozenLake(Mode):
         self.add_mode_event_handler('room_2f_choose_next_shot', self.event_add_a_shot)
         self.event_add_a_shot(**kwargs)
 
+        player = self.machine.game.player
+
+        # if shot sequence is 0 characters long then reset pattern to default
+        if len(player['room_2f_target_order']) == 0:
+            player['room_2f_target_order'] = 'ABCDEFGHIJKLMN'
+
+        # if shot sequence is 14 characters long
+        #    shuffle first 10
+        #    shuffle last 4
+
         super().mode_start(**kwargs)
 
     def event_add_a_shot(self, **kwargs):
+        # only if shots are left:
+        #   pop off first character
+        #   subtract 65 and save as next shot
+        #   save abbreviated shot list
         self.machine.events.post('room_2f_enable_shot_{}'.format(randint(1, 14)))
