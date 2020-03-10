608b164bc8925ef74ca7726f35617f2e7d8c8e4a
Mark Seiden <mwseiden@gmail.com>
2020-03-09 22:07:41 -0400

    Slow down animation

--- scriptlets/lights/backbox/heat_up.py
+++ scriptlets/lights/backbox/heat_up.py
@@ -15,21 +15,22 @@ class HeatUp(DynamicBackBoxShow):
     def animate(self):
         super().animate()
 
-        for strip_number in range(self.strip_count):
-            for index in range(10):
-                value = (self.add_percentage * 10) - ((index + 1) * self.add_percentage) + (self.current_step * self.add_percentage)
- 
-                if value > 100:
-                    value = 100
+        if self.frame % 5 == 0:
+            for strip_number in range(self.strip_count):
+                for index in range(10):
+                    value = (self.add_percentage * 10) - ((index + 1) * self.add_percentage) + (self.current_step * self.add_percentage)
+     
+                    if value > 100:
+                        value = 100
 
-                r, g, b = self.color.rgb
-                r = r * value / 100
-                g = g * value / 100
-                b = b * value / 100
+                    r, g, b = self.color.rgb
+                    r = r * value / 100
+                    g = g * value / 100
+                    b = b * value / 100
 
-                self.strips[strip_number].set_color(index, RGBColor([r,g,b]))
-   
-                self.current_step = self.current_step + 1 
+                    self.strips[strip_number].set_color(index, RGBColor([r,g,b]))
+       
+                    self.current_step = self.current_step + 1 
 
 
     def is_finished(self):
