28e3e32b937504df1ec922692ca0dabd8e823abb
Mark Seiden <mwseiden@gmail.com>
2020-04-15 19:54:17 -0400

    Add map mode and some docs

---
+++ modes/map/code/map.py
@@ -0,0 +1,10 @@
+from mpf.core.mode import Mode
+
+class Map(Mode):
+
+    def mode_start(self, **kwargs):
+        super().mode_start(**kwargs)
+
+    def mode_stop(self, **kwargs):
+        super().mode_stop(**kwargs)
+
