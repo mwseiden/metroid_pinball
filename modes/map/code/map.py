234293df8913df8ddc2e968406f312ab5233f1e1
Mark Seiden <mwseiden@gmail.com>
2020-04-20 23:47:08 -0400

    Add discovered vs undiscovered

--- modes/map/code/map.py
+++ modes/map/code/map.py
@@ -15,17 +15,17 @@ class Map(Mode):
     # door order: N - E - W - S
 
     LAYOUT = {
-      '1a': ['C', 1, 1, 0, 0, None, 1, None, 0, None, 2, 'door_1a_1f'],
-      '1b': ['C', 2, 2, 0, 0, None, 1, None, 0, None, 0, None],
-      '1c': ['C', 3, 3, 0, 0, None, 1, None, 0, None, 4, None],
-      '1d': ['C', 4, 4, 0, 0, None, 4, None, 0, None, 0, None],
-      '1e': ['C', 5, 0, 1, 0, None, 1, None, 4, None, 0, None],
-      '1f': ['C', 6, 1, 1, 0, None, 1, None, 1, None, 4, None],
-      '1g': ['C', 7, 2, 1, 0, None, 1, None, 0, None, 0, None]
+      '1a': ['c', 1, 1, 0, 0, None, 1, None, 0, None, 2, 'door_1a_1f'],
+      '1b': ['c', 2, 2, 0, 0, None, 1, None, 0, None, 0, None],
+      '1c': ['c', 3, 3, 0, 0, None, 1, None, 0, None, 4, None],
+      '1d': ['c', 4, 4, 0, 0, None, 4, None, 0, None, 0, None],
+      '1e': ['c', 5, 0, 1, 0, None, 1, None, 4, None, 0, None],
+      '1f': ['c', 6, 1, 1, 0, None, 1, None, 1, None, 4, None],
+      '1g': ['c', 7, 2, 1, 0, None, 1, None, 0, None, 0, None]
     }
 
     AREAS = {
-      'C': [192, 96, ['1a', '1b', '1c', '1d', '1e', '1f', '1g']]
+      'c': [192, 96, ['1a', '1b', '1c', '1d', '1e', '1f', '1g']]
     }
 
     DOOR_TYPES = {
@@ -52,18 +52,23 @@ class Map(Mode):
 
     def event_set_location(self, **kwargs):
         room_code = kwargs.get('room', '1b')
-        visit = kwargs.get('visit', False)
+
+        if kwargs.get('visit', False):
+            player_var = 'map_visited_{}'.format(self.LAYOUT[room_code][0])
+            current_visits = list(self.player[player_var])
+            current_visits[self.LAYOUT[room_code][1] - 1] = 'Y'
+            self.player[player_var] = "".join(current_visits)
 
         self.player.map_location = room_code
-        self.draw_map()
+        self.draw_map('c')
 
-    def draw_map(self):
+    def draw_map(self, area_code):
         self.remove_background()
-        self.draw_background('C')
+        self.draw_background(area_code)
 
         self.remove_player()
 
-        for room_code in self.AREAS['C'][2]:
+        for room_code in self.AREAS[area_code][2]:
             self.draw_map_tile(room_code)
 
     def draw_map_tile(self, room_code):
@@ -78,7 +83,7 @@ class Map(Mode):
         self.remove_exit(room_number, 'w')
         self.remove_exit(room_number, 's')
 
-        self.draw_room(room_number, x, y, 'x')
+        self.draw_room(room_number, x, y, list(self.player['map_visited_{}'.format(self.LAYOUT[room_code][0])])[room_number - 1] == 'Y')
         self.draw_exit(room_number, 'n', x, y + 14, exit_n_type, exit_n_var)
         self.draw_exit(room_number, 'e', x + 18, y, exit_e_type, exit_e_var)
         self.draw_exit(room_number, 'w', x - 14, y, exit_w_type, exit_w_var)
@@ -126,7 +131,7 @@ class Map(Mode):
               'x': x,
               'y': y,
               'z': 1001,
-              'image': 'map_tile_undiscovered'
+              'image': 'map_tile_discovered' if room_type else 'map_tile_undiscovered'
             }
           }
         }
