ee837e1a9360a0b08a088deca51c03114d67dbac
Mark Seiden <mwseiden@gmail.com>
2020-04-26 10:36:31 -0400

    Add room types

--- modes/map/code/map.py
+++ modes/map/code/map.py
@@ -15,58 +15,58 @@ class Map(Mode):
     # door order: N - E - W - S
 
     LAYOUT = {
-      '1a': ['c', 1, 1, 0, 0, None, 1, None, 0, None, 2, 'door_1a_1f'],
-      '1b': ['c', 2, 2, 0, 0, None, 1, None, 0, None, 0, None],
-      '1c': ['c', 3, 3, 0, 0, None, 1, None, 0, None, 4, None],
-      '1d': ['c', 4, 4, 0, 0, None, 4, None, 0, None, 0, None],
-      '1e': ['c', 5, 0, 1, 0, None, 1, None, 4, None, 0, None],
-      '1f': ['c', 6, 1, 1, 0, None, 1, None, 1, None, 4, None],
-      '1g': ['c', 7, 2, 1, 0, None, 0, None, 0, None, 0, None],
+      '1a': ['c', 1, 0, 1, 0, 0, None, 1, None, 0, None, 2, 'door_1a_1f'],
+      '1b': ['c', 2, 0, 2, 0, 0, None, 1, None, 0, None, 0, None],
+      '1c': ['c', 3, 0, 3, 0, 0, None, 1, None, 0, None, 4, None],
+      '1d': ['c', 4, 0, 4, 0, 0, None, 4, None, 0, None, 0, None],
+      '1e': ['c', 5, 0, 0, 1, 0, None, 1, None, 4, None, 0, None],
+      '1f': ['c', 6, 0, 1, 1, 0, None, 1, None, 1, None, 4, None],
+      '1g': ['c', 7, 0, 2, 1, 0, None, 0, None, 0, None, 0, None],
 
-      '1h': ['w', 1, 0, 0, 0, None, 1, None, 0, None, 0, None],
-      '1i': ['w', 2, 1, 0, 0, None, 1, None, 0, None, 1, None],
-      '1j': ['w', 3, 2, 0, 0, None, 0, None, 0, None, 0, None],
-      '1k': ['w', 4, 1, 1, 0, None, 0, None, 0, None, 1, None],
-      '1l': ['w', 5, 1, 2, 0, None, 1, None, 4, None, 1, None],
-      '1m': ['w', 6, 2, 2, 0, None, 0, None, 0, None, 0, None],
-      '1n': ['w', 7, 1, 3, 0, None, 0, None, 0, None, 1, None],
-      '1o': ['w', 8, 0, 4, 0, None, 1, None, 0, None, 0, None],
-      '1p': ['w', 9, 1, 4, 0, None, 1, None, 0, None, 4, None],
-      '1q': ['w', 10, 2, 4, 0, None, 0, None, 0, None, 0, None],
+      '1h': ['w', 1, 0, 0, 0, 0, None, 1, None, 0, None, 0, None],
+      '1i': ['w', 2, 0, 1, 0, 0, None, 1, None, 0, None, 1, None],
+      '1j': ['w', 3, 0, 2, 0, 0, None, 0, None, 0, None, 0, None],
+      '1k': ['w', 4, 0, 1, 1, 0, None, 0, None, 0, None, 1, None],
+      '1l': ['w', 5, 0, 1, 2, 0, None, 1, None, 4, None, 1, None],
+      '1m': ['w', 6, 0, 2, 2, 0, None, 0, None, 0, None, 0, None],
+      '1n': ['w', 7, 0, 1, 3, 0, None, 0, None, 0, None, 1, None],
+      '1o': ['w', 8, 0, 0, 4, 0, None, 1, None, 0, None, 0, None],
+      '1p': ['w', 9, 0, 1, 4, 0, None, 1, None, 0, None, 4, None],
+      '1q': ['w', 10, 0, 2, 4, 0, None, 0, None, 0, None, 0, None],
 
-      '1r': ['b', 1, 1, 0, 4, None, 1, None, 0, None, 1, None],
-      '1s': ['b', 2, 2, 0, 0, None, 1, None, 0, None, 0, None],
-      '1t': ['b', 3, 3, 0, 4, None, 0, None, 0, None, 1, None],
-      '1u': ['b', 4, 1, 1, 0, None, 0, None, 0, None, 1, None],
-      '1v': ['b', 5, 3, 1, 0, None, 0, None, 0, None, 1, None],
-      '1w': ['b', 6, 1, 2, 0, None, 1, None, 0, None, 0, None],
-      '1x': ['b', 7, 2, 2, 0, None, 1, None, 0, None, 0, None],
-      '1y': ['b', 8, 3, 2, 0, None, 4, None, 0, None, 0, None],
+      '1r': ['b', 1, 0, 1, 0, 4, None, 1, None, 0, None, 1, None],
+      '1s': ['b', 2, 0, 2, 0, 0, None, 1, None, 0, None, 0, None],
+      '1t': ['b', 3, 0, 3, 0, 4, None, 0, None, 0, None, 1, None],
+      '1u': ['b', 4, 0, 1, 1, 0, None, 0, None, 0, None, 1, None],
+      '1v': ['b', 5, 0, 3, 1, 0, None, 0, None, 0, None, 1, None],
+      '1w': ['b', 6, 0, 1, 2, 0, None, 1, None, 0, None, 0, None],
+      '1x': ['b', 7, 0, 2, 2, 0, None, 1, None, 0, None, 0, None],
+      '1y': ['b', 8, 0, 3, 2, 0, None, 4, None, 0, None, 0, None],
 
-      '1z': ['m', 1, 1, 0, 0, None, 1, None, 0, None, 1, None],
-      '2a': ['m', 2, 2, 0, 4, None, 1, None, 0, None, 0, None],
-      '2b': ['m', 3, 3, 0, 0, None, 0, None, 0, None, 1, None],
-      '2c': ['m', 4, 0, 1, 0, None, 1, None, 4, None, 1, None],
-      '2d': ['m', 5, 1, 1, 0, None, 2, 'door_2d_2e', 0, None, 1, None],
-      '2e': ['m', 6, 2, 1, 0, None, 2, 'door_2e_2f', 0, None, 0, None],
-      '2f': ['m', 7, 3, 1, 0, None, 0, None, 0, None, 0, None],
-      '2g': ['m', 8, 0, 2, 0, None, 1, None, 0, None, 0, None],
-      '2h': ['m', 9, 1, 2, 0, None, 0, None, 0, None, 0, None],
+      '1z': ['m', 1, 0, 1, 0, 0, None, 1, None, 0, None, 1, None],
+      '2a': ['m', 2, 0, 2, 0, 4, None, 1, None, 0, None, 0, None],
+      '2b': ['m', 3, 0, 3, 0, 0, None, 0, None, 0, None, 1, None],
+      '2c': ['m', 4, 0, 0, 1, 0, None, 1, None, 4, None, 1, None],
+      '2d': ['m', 5, 0, 1, 1, 0, None, 2, 'door_2d_2e', 0, None, 1, None],
+      '2e': ['m', 6, 0, 2, 1, 0, None, 2, 'door_2e_2f', 0, None, 0, None],
+      '2f': ['m', 7, 0, 3, 1, 0, None, 0, None, 0, None, 0, None],
+      '2g': ['m', 8, 0, 0, 2, 0, None, 1, None, 0, None, 0, None],
+      '2h': ['m', 9, 0, 1, 2, 0, None, 0, None, 0, None, 0, None],
 
-      '2i': ['n', 1, 1, 0, 0, None, 1, None, 0, None, 1, None],
-      '2j': ['n', 2, 2, 0, 0, None, 0, None, 0, None, 0, None],
-      '2k': ['n', 3, 3, 0, 0, None, 0, None, 0, None, 1, None],
-      '2l': ['n', 4, 0, 1, 0, None, 1, None, 0, None, 0, None],
-      '2m': ['n', 5, 1, 1, 0, None, 1, None, 0, None, 0, None],
-      '2n': ['n', 6, 2, 1, 0, None, 1, None, 0, None, 1, None],
-      '2o': ['n', 7, 3, 1, 0, None, 1, None, 0, None, 0, None],
-      '2p': ['n', 8, 4, 1, 0, None, 0, None, 0, None, 0, None],
-      '2q': ['n', 9, 2, 2, 0, None, 0, None, 0, None, 0, None],
+      '2i': ['n', 1, 0, 1, 0, 4, None, 1, None, 0, None, 1, None],
+      '2j': ['n', 2, 0, 2, 0, 0, None, 0, None, 0, None, 0, None],
+      '2k': ['n', 3, 0, 3, 0, 0, None, 0, None, 0, None, 1, None],
+      '2l': ['n', 4, 0, 0, 1, 0, None, 1, None, 0, None, 0, None],
+      '2m': ['n', 5, 0, 1, 1, 0, None, 1, None, 0, None, 0, None],
+      '2n': ['n', 6, 0, 2, 1, 0, None, 1, None, 0, None, 1, None],
+      '2o': ['n', 7, 0, 3, 1, 0, None, 1, None, 0, None, 0, None],
+      '2p': ['n', 8, 0, 4, 1, 0, None, 0, None, 0, None, 0, None],
+      '2q': ['n', 9, 0, 2, 2, 0, None, 0, None, 0, None, 0, None],
 
-      '2r': ['t', 1, 0, 0, 0, None, 1, None, 0, None, 0, None],
-      '2s': ['t', 2, 1, 0, 0, None, 1, None, 0, None, 0, None],
-      '2t': ['t', 3, 2, 0, 0, None, 1, None, 0, None, 0, None],
-      '2u': ['t', 4, 3, 0, 0, None, 4, None, 0, None, 0, None]
+      '2r': ['t', 1, 0, 0, 0, 0, None, 1, None, 0, None, 0, None],
+      '2s': ['t', 2, 0, 1, 0, 0, None, 1, None, 0, None, 0, None],
+      '2t': ['t', 3, 0, 2, 0, 0, None, 1, None, 0, None, 0, None],
+      '2u': ['t', 4, 0, 3, 0, 0, None, 4, None, 0, None, 0, None]
     }
 
     AREAS = {
@@ -91,6 +91,18 @@ class Map(Mode):
       'e': 'h',
       'w': 'h',
       's': 'v'
+    }
+
+    ROOM_TYPES = {
+      0: 'normal',
+      1: 'boss',
+      2: 'secret'
+    }
+
+    ROOM_STATES = {
+      'N': 'undiscovered',
+      'Y': 'discovered',
+      'C': 'completed'
     }
 
     def mode_start(self, **kwargs):
@@ -131,16 +143,16 @@ class Map(Mode):
             self.draw_map_tile(room_code)
 
     def draw_map_tile(self, room_code):
-        area_code, room_number, x, y, exit_n_type, exit_n_var, exit_e_type, exit_e_var, exit_w_type, exit_w_var, exit_s_type, exit_s_var = self.LAYOUT.get(room_code)
+        area_code, room_number, room_type, x, y, exit_n_type, exit_n_var, exit_e_type, exit_e_var, exit_w_type, exit_w_var, exit_s_type, exit_s_var = self.LAYOUT.get(room_code)
 
         x = self.ORIGIN_X - self.AREAS[area_code][0] + 24 + (x * 32)
         y = self.ORIGIN_Y - 24 - (y * 32)
 
-        self.draw_room(room_number, x, y, list(self.player['map_visited_{}'.format(self.LAYOUT[room_code][0])])[room_number - 1] == 'Y')
+        self.draw_room(room_number, x, y, room_type, list(self.player['map_visited_{}'.format(self.LAYOUT[room_code][0])])[room_number - 1])
         self.draw_exit(room_number, 'n', x, y + 14, exit_n_type, exit_n_var)
-        self.draw_exit(room_number, 'e', x + 14, y, exit_e_type, exit_e_var)
+        self.draw_exit(room_number, 'e', x + 16, y, exit_e_type, exit_e_var)
         self.draw_exit(room_number, 'w', x - 14, y, exit_w_type, exit_w_var)
-        self.draw_exit(room_number, 's', x, y - 14, exit_s_type, exit_s_var)
+        self.draw_exit(room_number, 's', x, y - 16, exit_s_type, exit_s_var)
 
         if self.player.map_location == room_code:
             self.draw_player(x, y)
@@ -174,7 +186,7 @@ class Map(Mode):
 
         self.machine.widget_player.play(settings, 'map', None)
 
-    def draw_room(self, room_number, x, y, room_type):
+    def draw_room(self, room_number, x, y, room_type, room_state):
         settings = {
           'map_tile_{}'.format(room_number): {
             'action': 'add',
@@ -184,7 +196,7 @@ class Map(Mode):
               'x': x,
               'y': y,
               'z': 1001,
-              'image': 'map_tile_discovered' if room_type else 'map_tile_undiscovered'
+              'image': 'map_tile_{}_{}'.format(self.ROOM_TYPES[room_type], self.ROOM_STATES[room_state])
             }
           }
         }
