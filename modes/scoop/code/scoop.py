c07660ed8dc0c58b391286cfc86a1c83517c4427
Mark Seiden <mwseiden@gmail.com>
2021-02-15 21:40:27 -0500

    Add landing sequence logic to scoop code

--- modes/scoop/code/scoop.py
+++ modes/scoop/code/scoop.py
@@ -3,16 +3,30 @@ from mpf.core.mode import Mode
 class Scoop(Mode):
 
     def mode_start(self, **kwargs):
-        # self.current_music = None
-        # self.current_overlay_music = None
-
-        # self.add_mode_event_handler('cmd_play_map_music', self.event_play_map_music)
-        # self.add_mode_event_handler('cmd_play_overlay_music', self.event_play_overlay_music)
         self.add_mode_event_handler('ball_hold_collect_award_shot_held_ball', self.event_ball_collected)
+        self.add_mode_event_handler('cmd_scoop_collect', self.event_scoop_hold_collect)
+        # self.add_mode_event_handler('cmd_scoop_clear', self.event_scoop_clear)
 
         super().mode_start(**kwargs)
 
 
     def event_ball_collected(self, **kwargs):
+        player = self.machine.game.player
+
+        if player['scoop_collectables'][0] == '1':
+            self.machine.events.post('scoop_award_land_the_gunship')
+            self._clear_collectable(0)
+
         self.machine.events.post('scoop_award_collected')
 
+    def event_scoop_hold_collect(self, **kwargs):
+        self._set_collectable(kwargs.get('index', 0))
+
+    def event_scoop_clear(self, **kwargs):
+        self._clear_collectable(kwargs.get('index', 0))
+
+    def _set_collectable(self, index):
+        self.machine.game.player['scoop_collectables'] = player[:index] + '1' + string[index+1:]
+
+    def _clear_collectable(self, index):
+        self.machine.game.player['scoop_collectables'] = player[:index] + '0' + string[index+1:]
