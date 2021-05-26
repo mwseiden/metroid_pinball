689f70324e3d9f44c05aa2cc61d0d6ef1490de86
Mark Seiden <mwseiden@gmail.com>
2021-05-26 19:58:11 -0400

    Add color wheel code

--- scriptlets/lights/backbox/wheel.py
+++ scriptlets/lights/backbox/wheel.py
@@ -7,14 +7,3 @@ class Wheel:
         color = hsv_to_rgb(wheel_pos / 255, 1.0, 1.0)
         return RGBColor([int(color[0] * 255), int(color[1] * 255), int(color[2] * 255)])
 
-
-    def at_old(self, wheel_pos):
-
-        if wheel_pos < 85:
-            return RGBColor([(wheel_pos * 3) % 255, 255 - (wheel_pos % 255), 0])
-        elif wheel_pos < 170:
-            wheel_pos -= 85;
-            return RGBColor([255 - ((wheel_pos * 3) % 255), 0, (wheel_pos * 3) % 255])
-        else:
-            wheel_pos -= 170;
-            return RGBColor([0, (wheel_pos * 3) % 255, 255 - ((wheel_pos * 3) % 255)])
