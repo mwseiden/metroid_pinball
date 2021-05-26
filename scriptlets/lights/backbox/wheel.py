0b1cdf39ba2123120b14cc3524d54965cd7bbe12
Mark Seiden <mwseiden@gmail.com>
2021-05-26 19:41:08 -0400

    Use native python hsv code

--- scriptlets/lights/backbox/wheel.py
+++ scriptlets/lights/backbox/wheel.py
@@ -1,8 +1,14 @@
+from colorsys import hsv_to_rgb
 from mpf.core.rgb_color import RGBColor
 
 class Wheel:
 
     def at(self, wheel_pos):
+        color = hsv_to_rgb(wheel_pos / 255, 1.0, 1.0)
+        return RGBColor(color)
+
+
+    def at_old(self, wheel_pos):
 
         if wheel_pos < 85:
             return RGBColor([(wheel_pos * 3) % 255, 255 - (wheel_pos % 255), 0])
